831182dbb952b6092fa63587cd38fdf6038b8c20
azznek <kenzadahhmani@gmail.com>
2026-02-03 19:34:29 +0100

    Add Header component to App layout

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,7 @@ import Footer from './components/Footer';
 function App() {
   return (
     <div className="min-h-screen bg-slate-950">
+      <Header />
       <Hero />
       <About />
       <ChronoQuiz />
